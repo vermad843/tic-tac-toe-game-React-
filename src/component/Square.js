ac43057ac5e9628f94096c80da222bbaa5555aed
vermad843 <vishnuverma7524@gmail.com>
2019-10-18 12:06:02 +0530

    adding onClick event

--- src/component/Square.js
+++ src/component/Square.js
@@ -4,7 +4,8 @@ import React, { Component } from 'react';
 class Square extends React.Component {
     render() {
       return (
-        <button className="square">
+        <button className="square" 
+        onClick = {() => alert('click') }>
           {this.props.value}
         </button>
       );
