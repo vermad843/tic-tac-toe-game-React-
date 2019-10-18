44be2e2e69f302e92712dd91467685cc6826d579
vermad843 <vishnuverma7524@gmail.com>
2019-10-18 19:02:35 +0530

    this.handleClick is not a function error

--- src/component/Square.js
+++ src/component/Square.js
@@ -2,17 +2,11 @@ import React,{Component} from 'react';
 
 
 class Square extends React.Component {
-  constructor(props){
-    super(props); 
-     this.state = {
-       value : null,
-     };
-  }
     render() {
       return (
         <button className="square" 
         onClick = {() => 
-          this.setState({value : 'X'})}>
+          this.props.onClick()}>
           {this.props.value}
         </button>
       );
